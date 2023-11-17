5ba003eae48d23310017e0e76388dca7d7891dc7
Castro <103647024+joaoguicastro@users.noreply.github.com>
2023-11-17 14:52:57 -0300

    teste

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,10 @@
-import { REFUSED } from 'dns'
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
     client: 'sqlite',
     connection: {
-        filename: './db/app.db',
+        filename: env.DATABASE_URL,
     },
     useNullAsDefault: true,
     migrations: {
@@ -18,3 +18,5 @@ export const knex = setupKnex(config)
 // para fazer a criaçao da migrate no knex e o npm run knex -- migrate:make create-documents
 // executar o servidor npm run dev
 // help do knex e npm run knex -- -h
+// rodar a migraçao npm run knex -- migrate:latest
+// precisa do eslint como extensao do visual studio
