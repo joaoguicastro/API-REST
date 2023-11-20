907bc50ba7d6b08e819c199330f0280786835869
Castro <103647024+joaoguicastro@users.noreply.github.com>
2023-11-19 23:18:21 -0300

    estudo para o api rest

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,17 +1,28 @@
 import { FastifyInstance } from "fastify"
+import { z } from 'zod'
 import { knex } from "../database"
+import crypto from 'node:crypto'
 
 export async function transactionsRoutes(app:FastifyInstance){
-    app.get('/hello', async () => {
-        // const transactions = await knex('transactions').insert({
-        //   id: crypto.randomUUID(),
-        //   title: 'Transação de teste',
-        //   amount: 1000,
-      
-        // }).returning('*')
-      
-        const transactions = await knex('transactions').where('amount', 1000).select('*')
-      
-        return transactions
-      })
+  app.post('/', async (request, reply) => {
+    // {title, amount, type: credit ou debit}
+
+    const createTransactionBodySchema =z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount *-1,
+    })
+
+    //HTTP Codes(201 - recurso criado com sucesso)
+
+    return reply.status(201).send()
+  })
 }
